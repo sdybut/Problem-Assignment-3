3b7abd758aa208e76df3c0c205cfb4a0a484b8aa
sdybut <ybutsteph@gmail.com>
2025-09-16 22:49:52 +0800

    Add files via upload

--- YBUT_Pandas-P1.py
+++ YBUT_Pandas-P1.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Converted from Jupyter Notebook: notebook.ipynb
-Conversion Date: 2025-09-16T14:30:22.503Z
+Converted from Jupyter Notebook: YBUT_Pandas-P2 (2) (1).ipynb
+Conversion Date: 2025-09-16T14:49:10.548Z
 """
 
-# # Programming Assignment 3
+# # **Programming Assignment 3**
 
 
-import pandas as pd
+# ## **Problem 1**
 
-soosh = pd.read_csv ('cars.csv')
-soosh
 
-soosh.head (5)
+import pandas as pd # import the Pandas library with the 'pd'
 
-soosh.tail (5)
+cars = pd.read_csv ('cars.csv') # Read a CSV file into a pandas DataFrame
+cars #print
+
+cars.head (5) # print the first 5 rows
+
+cars.tail (5) # print the last 5 rows
